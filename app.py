981f1cc6d11808c26c99effaab32cd2aefc5f16b
K Bhawana Sai <128051273+Bhawanakalaga@users.noreply.github.com>
2025-05-14 23:58:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+import os
+
 
 app = Flask(__name__)
 
@@ -18,5 +20,7 @@ def predict():
     return jsonify({'prediction': int(prediction[0])})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port, debug=True)
+    
 
