3d7bca431d7f2a3740936c7a9ba32d60f85d7c16
K Bhawana Sai <128051273+Bhawanakalaga@users.noreply.github.com>
2025-06-02 23:44:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = joblib.load('model.pkl')
 
 @app.route('/')
 def home():
-    return render('index.html')  # serves index.html
+    return render_template('index.html')  # serves index.html
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -21,6 +21,7 @@ def predict():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
 
     
 
