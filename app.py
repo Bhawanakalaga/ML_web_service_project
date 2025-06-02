031085280cdbe570bf34d06db4b3ba73484f6a4a
K Bhawana Sai <128051273+Bhawanakalaga@users.noreply.github.com>
2025-06-02 23:16:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
 import os
-
 
 app = Flask(__name__)
 
@@ -10,17 +9,18 @@ model = joblib.load('model.pkl')
 
 @app.route('/')
 def home():
-    return "Welcome to the Iris Prediction API!"
+    return render_template('index.html')  # serves index.html
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json(force=True)
+    data = request.json
     features = np.array(data['features']).reshape(1, -1)
     prediction = model.predict(features)
     return jsonify({'prediction': int(prediction[0])})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
     
 
