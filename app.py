af8fe14882f11027357e1bcb12fb9250070b6d64
K Bhawana Sai <128051273+Bhawanakalaga@users.noreply.github.com>
2025-06-03 00:01:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ import os
 
 app = Flask(__name__)
 
+# Load the trained model
 model = joblib.load('model.pkl')
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # serves index.html
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -21,6 +22,7 @@ def predict():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
     
